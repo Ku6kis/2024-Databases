7dcd4e7a78672e0bea49b557b75b05acd6be7151
Kasperi Kukkamaki <kukkamakikasperi@gmail.com>
2024-10-24 19:10:33 +0300

    siirto

--- Exercise 3/task2.sql
+++ Exercise 3/task2.sql
@@ -1,7 +1,25 @@
 -- In task 2 you will need GROUP BY, ORDER BY ja COUNT commands.
 
--- Find out which Northwind company's employees have sold products and how many. 
+-- Find out which Northwind company's employees have sold products and how many.
 -- Include the count of sold orders, employee's name and title in the company to
--- the result. Order the result according the number of sales in descending order. 
+-- the result. Order the result according the number of sales in descending order.
 -- Don't include employees that have not sold anything in the result.
 
+-- ESIMERKKI  INSERT INTO `employees` (`id`, `company`, `last_name`, `first_name`, `email_address`, `job_title`, `business_phone`, `home_phone`, `mobile_phone`, `fax_number`, `address`, `city`, `state_province`, `zip_postal_code`, `country_region`, `web_page`, `notes`, `attachments`) VALUES (1, 'Northwind Traders', 'Freehafer', 'Nancy', 'nancy@northwindtraders.com', 'Sales Representative', '(123)555-0100', '(123)555-0102', NULL, '(123)555-0103', '123 1st Avenue', 'Seattle', 'WA', '99999', 'USA', '#http://northwindtraders.com#', NULL, '');
+
+-- ESIMERKKI  INSERT INTO `orders` (`id`, `employee_id`, `customer_id`, `order_date`, `shipped_date`, `shipper_id`, `ship_name`, `ship_address`, `ship_city`, `ship_state_province`, `ship_zip_postal_code`, `ship_country_region`, `shipping_fee`, `taxes`, `payment_type`, `paid_date`, `notes`, `tax_rate`, `tax_status_id`, `status_id`) VALUES (30, 9, 27, '2006-01-15 00:00:00', '2006-01-22 00:00:00', 2, 'Karen Toh', '789 27th Street', 'Las Vegas', 'NV', '99999', 'USA', 200, 0, 'Check', '2006-01-15 00:00:00', NULL, 0, NULL, 3);
+
+
+SELECT
+    COUNT(orders.id) AS order_number,
+    employees.first_name AS first_name,
+    employees.last_name AS last_name,
+    employees.job_title as title
+FROM
+    employees
+JOIN
+    orders ON employees.id = orders.employee_id
+GROUP BY
+    employees.id
+ORDER BY
+    order_number DESC;
--- Exercise 3/task4.sql
+++ Exercise 3/task4.sql
@@ -1,4 +1,14 @@
 -- Northwind company has many products for sale. Find out how many orders include
--- each of the products. Write a query which returns all company's products 
--- (the name of the product) and the number of orders. Include the products which are 
+-- each of the products. Write a query which returns all company's products
+-- (the name of the product) and the number of orders. Include the products which are
 -- not part of any order in the result.
+
+-- ESIMERKKI INSERT INTO `products` (`supplier_ids`, `id`, `product_code`, `product_name`, `description`, `standard_cost`, `list_price`, `reorder_level`, `target_level`, `quantity_per_unit`, `discontinued`, `minimum_reorder_quantity`, `category`, `attachments`) VALUES ('4', 1, 'NWTB-1', 'Northwind Traders Chai', NULL, 13.5, 18, 10, 40, '10 boxes x 20 bags', 0, 10, 'Beverages', '');
+-- ESIMERKKI  INSERT INTO `orders` (`id`, `employee_id`, `customer_id`, `order_date`, `shipped_date`, `shipper_id`, `ship_name`, `ship_address`, `ship_city`, `ship_state_province`, `ship_zip_postal_code`, `ship_country_region`, `shipping_fee`, `taxes`, `payment_type`, `paid_date`, `notes`, `tax_rate`, `tax_status_id`, `status_id`) VALUES (30, 9, 27, '2006-01-15 00:00:00', '2006-01-22 00:00:00', 2, 'Karen Toh', '789 27th Street', 'Las Vegas', 'NV', '99999', 'USA', 200, 0, 'Check', '2006-01-15 00:00:00', NULL, 0, NULL, 3);
+
+
+SELECT
+    products.product_name AS product_name
+    COUNT(products.product_name) AS number_of_orders
+
+GROUP BY
