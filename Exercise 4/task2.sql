031bf3ff47f02a54c3f55ace5f96477938eae755
Kasperi Kukkamaki <kukkamakikasperi@gmail.com>
2025-01-30 00:53:33 +0200

    viimeisin

--- Exercise 4/task2.sql
+++ Exercise 4/task2.sql
@@ -17,3 +17,41 @@
 -- Normalize the table into the 3rd normal form by dividing columns and tables into smaller ones.
 -- Define new tables like the example above. No need to write CREATE statements.
 -- Add some example data to your table(s).
+
+-- Employee table:
+-- |id |name         |email              |
+-- --------------------------------------|
+-- |1  |Andy Smith   |andy@company.com   |
+-- |2  |Rudy Black   |rudy@company.com   |
+-- |3  |Santa Claus  |santa@company.com  |
+-- |4  |Han Solo     |solo@company.com   |
+-- |5  |James Bond   |james@company.com  |
+-- |6  |Patrik Laine |patrik@company.com |
+
+
+-- Office table
+-- |code  |city          |
+-- ----------------------|
+-- |B-11  |New York      |
+-- |A-22  |San Francisco |
+-- |C-44  |Kansas        |
+
+-- Employee office table
+-- |employee_id |office_code |
+-- --------------------------|
+-- |1           |B-11        |
+-- |2           |A-22        |
+-- |3           |B-11        |
+-- |4           |A-22        |
+-- |5           |C-44        |
+-- |6           |C-44        |
+
+-- Supervisor table:
+-- |employee_id |supervisor_id |
+-- ----------------------------|
+-- |1           |3             |
+-- |2           |4             |
+-- |3           |NULL          |
+-- |4           |NULL          |
+-- |5           |NULL          |
+-- |6           |5             |
